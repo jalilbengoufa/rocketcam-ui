42aa3385c5175de151db99893a55f1412f540061
Bengoufa Mohamed Abdeldjalil <mohamed-abdeldjalil.bengoufa.1@ens.etsmtl.ca>
2018-05-11 00:25:54 -0400

    use fetch instead and fix push function call

--- app.js
+++ app.js
@@ -3,25 +3,33 @@ new Vue({
     el: '#root',
     data: {
 
-        logs: []
+        logs: [],
+        interval: null,
     },
-    mounted() {
+    methods: {
 
+        loadLogs: function() {
+            var self = this;
 
+            setInterval(function() {
+                fetch('http://localhost:8000/')
+                    .then(function(response) {
+                        return response.json()
+                    }).then(function(json) {
+                        self.logs.push(json.toString());
+                    }).catch(function(ex) {
+                        console.log('parsing failed', ex)
+                    })
+            }.bind(this), 3000);
 
-
-
-      
-
+        }
     },
-    methods:{
 
-
-    	loadLogs:function(){
-    			axios.get('http://project.test/connect').then(response =>
-
-                this.logs.push(response.data));
-
-    	}.bind(this);
+    beforeDestroy: function() {
+        clearInterval(this.interval);
     }
+
+
 });
+
+/*               console.log(json.toString()),*/
