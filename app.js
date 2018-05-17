fcc4ea8309901b4b8ccac7d526bde3743a80864d
Bengoufa Mohamed Abdeldjalil <mohamed-abdeldjalil.bengoufa.1@ens.etsmtl.ca>
2018-05-16 23:39:56 -0400

    add logs ,bug to fix with GET request

--- app.js
+++ app.js
@@ -1,78 +1,84 @@
-//url : http://dangerous-monkey-85.localtunnel.me/
 new Vue({
 
     el: '#root',
     data: {
-        url : 'http://localhost:8000/',
+        url: 'https://cedille.localtunnel.me/',
         logs: [],
-        interval: null,
+        verifyConnection: null,
+        timeout: 0,
     },
     methods: {
-
-        loadLogs: function() {
+        connnexion: function() {
             var self = this;
-            //verify connection
-            fetch(self.url)
+            console.log(self.url)
+            fetch(self.url).then(function(response) {
+                    return response.json();
+                }).then(function(json) {
+                    if (json.connected == true) {
+                        self.logs.push('Connected to the Rocket!' + " [ " + new Date().toLocaleTimeString() + "   " + new Date().toLocaleDateString() + "  ]");
+                    }
+                }).catch(function(ex) {
+                    self.logs.push('Can not connect to the rocket , try again' + " [ " + new Date().toLocaleTimeString() + "   " + new Date().toLocaleDateString() + "  ]");
+                    console.log('parsing failed', ex);
+                  
+                })
+        },
+        start: function() {
+            var self = this;
+
+            fetch(self.url + 'start')
                 .then(function(response) {
                     return response.json()
                 }).then(function(json) {
-                   /* if (json.connected == true)
-                        self.logs.push('Connected to the Rocket'+ "    " + new Date())*/
-                       self.logs.push(json.toString() + " [ "+ new Date().toLocaleTimeString()+"   "+ new Date().toLocaleDateString()+"  ]"); 
+                    if (json.running == true) {
+                        self.logs.push('Recording started !' + " [ " + new Date().toLocaleTimeString() + "   " + new Date().toLocaleDateString() + "  ]");
+                    }
                 }).catch(function(ex) {
-                    console.log('parsing failed', ex)
-                })
+                    console.log('parsing failed', ex);
+                });
 
             //fetch in interval to verify the connection
-            setInterval(function() {
+            self.verifyConnection = setInterval(function() {
                 fetch(self.url)
                     .then(function(response) {
                         return response.json()
                     }).then(function(json) {
-                        if (json.running== false) {
-                            self.logs.push('not connected' + "    " + new Date());
+                        if (json.running == false) {
+                            self.timeout++;
                         }
-                        if (self.logs.length == 10) {
-                            self.logs.splice(0, 8)
+                        if (self.timeout == 3) {
+                            self.logs.push('The camera stopped running , please reload the page and try again' + " [ " + new Date().toLocaleTimeString() + "   " + new Date().toLocaleDateString() + "  ]");
+                            self.beforeDestroy();
+                            self.timeout = 0;
+                        }
+                        if (self.logs.length == 15) {
+                            self.logs.splice(0, 13)
                         }
                     }).catch(function(ex) {
-                        console.log('parsing failed', ex)
+                        self.logs.push('Connection lost please reload the page and try again' + " [ " + new Date().toLocaleTimeString() + "   " + new Date().toLocaleDateString() + "  ]");
+                        console.log('parsing failed', ex.toString())
+                        self.beforeDestroy();
                     })
-            }.bind(this), 1000);
-
-        },
-
-        start: function() {
-            var self = this;
-            fetch(self.url +'start')
-                .then(function(response) {
-                    return response.json()
-                }).then(function(json) {
-
-                    self.logs.push(json.toString() + "    " + new Date());
-
-                }).catch(function(ex) {
-                    console.log('parsing failed', ex)
-                });
+            }.bind(this), 5000);
 
         },
         stop: function() {
             var self = this;
-            fetch(self.url+'stop')
+            fetch(self.url + 'stop')
                 .then(function(response) {
                     return response.json()
                 }).then(function(json) {
-
-                    self.logs.push(json.toString() + "    " + new Date());
-
+                    if (json.running == false) {
+                        self.logs.push('Recording has been stopped !' + " [ " + new Date().toLocaleTimeString() + "   " + new Date().toLocaleDateString() + "  ]");
+                    }
                 }).catch(function(ex) {
-                    console.log('parsing failed', ex)
+                    console.log('parsing failed', ex.toString());
                 });
         },
         beforeDestroy: function() {
-            clearInterval(this.interval);
+            var self = this;
+            clearInterval(self.verifyConnection);
         }
     }
-
 
 });
