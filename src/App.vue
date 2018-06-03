f604c15c9bd71719a299083f0c31b36c6ff0e315
Bengoufa Mohamed Abdeldjalil <mohamed-abdeldjalil.bengoufa.1@ens.etsmtl.ca>
2018-06-02 23:22:52 -0400

    add events for button to update the UI

--- src/App.vue
+++ src/App.vue
@@ -9,35 +9,33 @@
                     </ul>
                 </b-col>
                 <b-col cols="12" md="8" order="2" no-gutters>
-                    <b-card no-body>
+                    <b-card style="background-color:#F5F5F5;" no-body>
                         <b-tabs pills card>
-                            <b-tab v-for="cam in Object.values(cams)" :key="cam.id" :title="'CAMERA '+cam.id">
-                                <h2>CAMERA {{cam.id}} </h2>
-                                <b-row>
+                            <b-tab v-for="cam in cams" :key="cam.id" :title="'CAMERA [ '+cam.id+' ]'">
+                                <h2>CAMERA {{'[ ' + cam.id + ' ]'}} </h2>
+                                <b-row class="no">
                                     <b-col>
                                         <b-img center width="400" v-bind:src="cam.picture" fluid alt="Fluid image" />
                                     </b-col>
                                     <b-col>
-                                        <b-button size="lg" variant="outline-primary">MODE:1</b-button>
-                                        <b-button size="lg" disabled variant="outline-primary">MODE:2</b-button>
+                                        <b-button v-on:click="changeToMode1(cam.index)" size="lg" :disabled="cam.mode1" variant="outline-primary">MODE:1</b-button>
+                                        <b-button v-on:click="changeToMode2(cam.index)" size="lg" :disabled="cam.mode2" variant="outline-primary">MODE:2</b-button>
                                         <br>
                                         <br>
-                                        <b-button size="lg" variant="warning">SNAPSHOT</b-button>
-                                        <b-button size="lg" variant="success">START</b-button>
-                                        <b-button size="lg" variant="danger">STOP</b-button>
+                                        <b-button v-if="cam.mode1" v-on:click="changeToMode2(cam.index)" size="lg" variant="warning">SNAPSHOT</b-button>
+                                        <b-button v-if="cam.mode2 && cam.start" v-on:click="start(cam.index,cam.id)" size="lg" variant="success">START</b-button>
+                                        <b-button v-if="cam.mode2 && cam.stop" v-on:click="stop(cam.index,cam.id)" size="lg" variant="danger">STOP</b-button>
                                         <br>
                                         <br>
                                         <h5>RECORDING STATE:</h5>
-                                        <div class="pulsating-circle"></div>
-                                        <br>
-                                        <br>
-                                        <span class="dot"></span>
+                                        <div v-if="cam.mode2 && cam.recording" class="pulsating-circle"></div>
+                                        <div v-if="cam.mode2 && cam.recording == false" class="dot"></div>
                                     </b-col>
                                 </b-row>
                             </b-tab>
                         </b-tabs>
                     </b-card>
-                    <b-card style="background-color:green;height: 300px " no-body>
+                    <b-card style="background-color:#F5F5F5;height: 330px" no-body>
                     </b-card>
                 </b-col>
             </b-row>
@@ -49,42 +47,57 @@ export default {
     name: 'app',
     data() {
         return {
-            cams: {
-                1: { id: 1 },
-                2: { id: 2 },
-                3: { id: 3 },
-                4: { id: 4 }
-            },
-            url: 'http://cedillepi.local/',
+            cams: [],
+            camsIds: [],
+            url: 'http://localhost:8000/',
             timeout: 0,
             logs: [],
-            verifyConnection: null
+            verifyConnection: null,
+            testbutton: true
         }
     },
-    mounted() {
+    async created() {
         var self = this;
-        /*      fetch(self.url).then(function(response) {
-                          return response.json();
-                      }).then(function(json) {
-                              self.cams = json.cams;///to continu
-                      }).catch(function(ex) {
-                          self.logs.unshift('Can not connect to the rocket , try again' + " [ " + new Date().toLocaleTimeString() + "  ]");
-                          console.log('parsing failed', ex);
-                        
-              });*/
-
-        Object.keys(self.cams).forEach(function(k) {
-            self.cams[k] = Object.assign({}, self.cams[k], { picture: "static/logo.png" });
-            self.cams[k] = Object.assign({}, self.cams[k], { connect: 'visible' })
-            self.cams[k] = Object.assign({}, self.cams[k], { start: 'invisible' })
-            self.cams[k] = Object.assign({}, self.cams[k], { stop: 'invisible' })
-            self.cams[k] = Object.assign({}, self.cams[k], { snapshot: 'visible' })
-        });
+        self.connnexion();
     },
     methods: {
-        connnexion: function(id) {
+        createCams: function() {
             var self = this;
+            var i;
+            for (i = 0; i < self.camsIds.length; i++) {
+                var camera = { id: self.camsIds[i], index: i, mode1: true, picture: "static/logo.png", mode2: false, start: true, stop: false, snapshot: true, recording: false };
+                self.cams.push(camera);
+            }
+        },
+        changeToMode1: function(id) {
+            var self = this;
+            self.cams[id].mode1 = true;
+            self.cams[id].mode2 = false;
+            self.stop(id);
+
+        },
+        changeToMode2: function(index) {
+            var self = this
+            console.log(self.cams[index].mode2)
+            self.cams[index].mode1 = false;
+            self.cams[index].mode2 = true;
+        },
+        connnexion: function() {
+            var self = this;
+
             fetch(self.url).then(function(response) {
+                return response.json()
+            }).then(function(json) {
+                self.camsIds = json
+                self.createCams()
+
+            }).catch(function(ex) {
+                self.logs.unshift('Can not connect to the rocket , try again' + " [ " + new Date().toLocaleTimeString() + "  ]")
+                console.log('parsing failed', ex)
+
+            });
+
+            /*fetch(self.url).then(function(response) {
                 return response.json();
             }).then(function(json) {
                 if (json.connected == true) {
@@ -99,61 +112,68 @@ export default {
                 self.logs.unshift('Can not connect to the rocket , try again' + " [ " + new Date().toLocaleTimeString() + "  ]");
                 console.log('parsing failed', ex);
 
-            })
+            })*/
         },
-        start: function(id) {
+        start: function(index, id) {
             var self = this;
+            self.cams[index].start = false;
+            self.cams[index].stop = true;
+            self.cams[index].recording = true;
 
-            fetch(self.url + 'start/' + id)
-                .then(function(response) {
-                    return response.json()
-                }).then(function(json) {
-                    if (json.running == true) {
-                        self.logs.unshift(json.stdout + " [ " + new Date().toLocaleTimeString() + "  ]");
-                        self.cams[id].start = 'invisible';
-                        self.cams[id].stop = 'visible';
-                    }
-                }).catch(function(ex) {
-                    console.log('parsing failed', ex);
-                });
+            /*  fetch(self.url + 'start/' + id)
+                  .then(function(response) {
+                      return response.json()
+                  }).then(function(json) {
+                      if (json.running == true) {
+                          self.logs.unshift(json.stdout + " [ " + new Date().toLocaleTimeString() + "  ]");
+                          self.cams[id].start = 'invisible';
+                          self.cams[id].stop = 'visible';
+                      }
+                  }).catch(function(ex) {
+                      console.log('parsing failed', ex);
+                  });
 
-            //fetch in interval to verify the connection
-            self.verifyConnection = setInterval(function() {
-                fetch(self.url)
-                    .then(function(response) {
-                        return response.json()
-                    }).then(function(json) {
-                        if (json.running == false) {
-                            self.timeout++;
-                        }
-                        if (self.timeout == 3) {
-                            self.logs.unshift('The camera stopped running , please reload the page and try again' + " [ " + new Date().toLocaleTimeString() + "  ]");
-                            self.cams[id].stop = 'invisible';
-                            self.beforeDestroy();
-                            self.timeout = 0;
-                        }
-                    }).catch(function(ex) {
-                        self.logs.unshift('Connection lost please reload the page and try again' + " [ " + new Date().toLocaleTimeString() + "  ]");
-                        console.log('parsing failed', ex.toString())
-                        self.beforeDestroy();
-                    })
-            }.bind(this), 5000);
+              //fetch in interval to verify the connection
+              self.verifyConnection = setInterval(function() {
+                  fetch(self.url)
+                      .then(function(response) {
+                          return response.json()
+                      }).then(function(json) {
+                          if (json.running == false) {
+                              self.timeout++;
+                          }
+                          if (self.timeout == 3) {
+                              self.logs.unshift('The camera stopped running , please reload the page and try again' + " [ " + new Date().toLocaleTimeString() + "  ]");
+                              self.cams[id].stop = 'invisible';
+                              self.beforeDestroy();
+                              self.timeout = 0;
+                          }
+                      }).catch(function(ex) {
+                          self.logs.unshift('Connection lost please reload the page and try again' + " [ " + new Date().toLocaleTimeString() + "  ]");
+                          console.log('parsing failed', ex.toString())
+                          self.beforeDestroy();
+                      })
+              }.bind(this), 1000);*/
         },
-        stop: function(id) {
+        stop: function(index, id) {
             var self = this;
-            fetch(self.url + 'stop/' + id)
-                .then(function(response) {
-                    return response.json()
-                }).then(function(json) {
-                    if (json.running == false) {
-                        self.logs.unshift(json.stdout + " [ " + new Date().toLocaleTimeString() + "  ]");
-                        self.cams[id].stop = 'invisible';
-                        self.cams[id].connect = 'visible';
-                        self.cams[id].snapshot = 'visible';
-                    }
-                }).catch(function(ex) {
-                    console.log('parsing failed', ex.toString());
-                });
+            self.cams[index].start = true;
+            self.cams[index].stop = false;
+            self.cams[index].recording = false;
+
+            /*   fetch(self.url + 'stop/' + id)
+                   .then(function(response) {
+                       return response.json()
+                   }).then(function(json) {
+                       if (json.running == false) {
+                           self.logs.unshift(json.stdout + " [ " + new Date().toLocaleTimeString() + "  ]");
+                           self.cams[id].stop = 'invisible';
+                           self.cams[id].connect = 'visible';
+                           self.cams[id].snapshot = 'visible';
+                       }
+                   }).catch(function(ex) {
+                       console.log('parsing failed', ex.toString());
+                   });*/
         },
         snapshot: function(id) {
             var self = this;
@@ -195,34 +215,38 @@ h2 {
     font-family: 'Share Tech Mono',
 }
 
-h1{
+h1 {
     color: #ffffff;
     margin-top: 10px;
     margin-bottom: 20px;
     text-shadow: 0 0 20px rgba(10, 175, 230, 1), 0 0 20px rgba(10, 175, 230, 0);
     font-family: 'Share Tech Mono',
 }
-.nav-pills .nav-link.active, .nav-pills .show>.nav-link {
+
+.nav-pills .nav-link.active,
+.nav-pills .show>.nav-link {
     color: white;
     background-color: #0f3854;
 }
+
 a {
     color: red;
 }
-a:hover{
+
+a:hover {
     color: green;
 }
 
 .no-gutters>.col,
 .no-gutters>[class*=col-] {
-    height: 900px;
+    height: 1000px;
     background: #0f3854;
-      background: radial-gradient(ellipse at center,  #0a2e38  0%, #000000 70%);
-
+    background: radial-gradient(ellipse at center, #0a2e38 0%, #000000 70%);
 }
 
 .container-fluid {
-    background-color: blue;
+    //background-color: #0f3854;
+    background: radial-gradient(ellipse at center, #0a2e38 0%, #000000 70%);
 }
 
 .card {
